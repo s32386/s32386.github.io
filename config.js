9f6bfe8bbd628697a917767b00a8ccc2acfb0bbd
s32386 <74299530+s32386@users.noreply.github.com>
2020-11-14 16:37:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+let ar = new ARboon(
+    [
+        new texture("AB2","https://s32386.github.io/ArChem/image/l0_i2.png"),
+        new texture("AB3","https://s32386.github.io/ArChem/image/l0_i3.png"),
+        new texture("AB2E2","https://s32386.github.io/ArChem/image/l0_i4.png"),
+        new texture("AB4","https://s32386.github.io/ArChem/image/l0_i5.png"),
+        new texture("AB3E","https://s32386.github.io/ArChem/image/l0_i6.png"),
+        new texture("AB5","https://s32386.github.io/ArChem/image/l0_i7.png"),
+        new texture("AB6","https://s32386.github.io/ArChem/image/l0_i8.png")
+    ]
+)
+ar.init()
--- main.js
+++ main.js
@@ -1,135 +1,263 @@
-const video = document.createElement("video");
-const canvasElement = document.getElementById("qr-canvas");
-const canvas = canvasElement.getContext("2d");
 
-const qrResult = document.getElementById("qr-result");
-
-var b = new boon();
-let o = new b.events();
-
-let jame = {
-  "7849-a625-60d-954a-b54976":{
-    title:"ท่ากบ (Breaststroke)",
-    image:"kob",
-    text:"1.กดมือพร้อมกับการกวาดมือไปด้านข้าง โดยกดแขนลงนั้นให้กดลงประมาณ 45 ํ\n2.โก่งแขนโดยงอข้อศอกและยกข้อศอกให้สูงเอาไว้พร้อมกับล็อคข้อศอกให้อยู่กับที่คือไม่ลากศอกออกไปด้านหลัง\n3. ตวัดมือทั้งสองข้างให้มาด้านหน้าในลักษณะกระพุ่มมือพร้อมทั้งให้หนีบศอกมาชิดตัวอย่างรวดเร็วพร้อมทั้งยืดแขนออกไปด้านหน้าอย่างเร็วด้วย\n4. ก้มหัว ส่งแรงจากไหล่ตามแขนไปด้วย"
-  },
-  "6468-39b1-586-854e-6cc455":{
-    title:"ท่ากรรเชียง (Backstroke)",
-    image:"kuncheang",
-    text:"1.ยืดแขนออกไปแล้วแขนต้องชิดหู\n2.กดแขนลงไปในน้ำที่สำคัญปล่อยไหล่ตามสบายถ้าเกร็งไหล่ให้อยู่กับที่เอาไว้จะกดแขนลงน้ำไม่ได้\n3.งอข้อศอกและตั้งมือพร้อมทั้งดันน้ำผ่านไปทางต้นขาอย่างรวดเร็ว\n4.จังหวะสุดท้ายของการดันน้ำให้กดมือลงอย่างแรงจนแขนของคุณตึง\n5.เมื่อยกแขนขึ้นมาจากน้ำต้องไม่งอข้อศอกและวางแขนไปด้านหลังโดยไม่ฟาดน้ำโดยการวางแขนนั้นให้หันฝ่ามือเอานิ้วก้อยลงก่อน"
-  },
-  "6748-a4b6-341-a414-ce9892":{
-    title:"ฟรีสไตล์ (Freestyle)",
-    image:"free",
-    text:"1.ยืดแขนไปด้านหน้าจนสุดแล้วแขนต้องชิดกับหู\n2.ต่อจากนั้นกดมือลงพร้อมกับโก่งแขนโดยการยกข้อศอกแรงที่จะส่งตัวนั้นจะออกมาจากไหล่\n3.ดันแขนท่อนล่างให้ผ่านไปใต้ลำตัวนิ้วทุกนิ้วเรียงชิดติดกัน\n4.ดันน้ำจนกระทั่งแขนตึงพอดี\n5.ยกแขนขึ้นโดยงอข้อศอกแล้ววาดแขนมาด้านหน้าวางมือลงน้ำแล้วยืดแขนออกไป"
-  },
-  "b368-32c7-930-888d-608dc1":{
-    title:"ท่าผีเสื้อ (Butterfly)",
-    image:"peesaut",
-    text:"1.เมื่อแขนอยู่ข้างหน้าให้กดมือลงพร้อมกับกวาดออกไปด้านข้างเล็กน้อย\n2.งอข้อศอกพร้อมทั้งดันมือผ่านใต้ลำตัว\n3.ดันน้ำจนแขนผ่านบริเวณต้นขา\n4.ยกแขนขึ้นให้ศอกและมือพ้นจากน้ำ\n5.วางแขนกลับไปด้านหน้าโดยให้แขนมีความกว้างเท่าช่วงไหล่"
-  }
+let texture  = function(marker,url){
+    this.url = url;
+    this.marker = marker;
+    this.buffer = null;
 }
-
-
-
-
-
-
-let scanning = false;
-let running = "start"
-let eeeee = null;
-let card = document.getElementById("card")
-
-
-let render = function(uuid){
-  let title = document.getElementById("title")
-  let image = document.getElementById("image")
-  let text  = document.getElementById("text")
-  if(jame[uuid]!=null){
-    console.log(uuid)
-    card.hidden = false;
-    title.innerHTML = jame[uuid]["title"]
-    image.src = "./image/" + jame[uuid]["image"] + ".gif"
-    text.innerHTML = jame[uuid]["text"]
-  }else{
-    card.hidden = true;
-  }
+let load_texture = class{
+    loader = null;
+    constructor(render){
+    //  (render instanceof ARboon.prototype._create_RUNNING_EVENT)
+    console.log(render)
+        this.render = render
+        console.log(this.render)
+        this.loader = this._create_loader();
+    }
+    _load_buffer (_texture_){
+        if(_texture_ instanceof texture)
+        return this.loader.load(_texture_.url,this.render.render())
+        else throw Error("ERROR : {} is not a texture object".replace("{}",_texture_))
+    }
+    _create_loader(){
+        THREE.TextureLoader()
+        THREE.TextureLoader.prototype.crossOrigin = '';
+        return new THREE.TextureLoader();
+    }
 }
-
-
-qrcode.callback = res => {
-  if (res) {
-    scanning = false;
-    render(res)
-  }
-};
-
-function tick() {
-  canvasElement.height = video.videoHeight;
-  canvasElement.width = video.videoWidth;
-  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-  try {eeeee.innerHTML = (!scanning) ? "start" : "stop"} catch (error) {}
-  requestAnimationFrame(tick);
-}
-
-function scan() {
-  try {
-    if(scanning) qrcode.decode();
-  } catch (e) {}
-  setTimeout(scan,100)
-}
-async function startCapture(displayMediaOptions) {
-  let captureStream = null;
-
-  try {
-    captureStream = await navigator.mediaDevices.getUserMedia(displayMediaOptions);
-  } catch(err) {
-    console.error("Error: " + err);
-  }
-  return captureStream;
-}
-
-try {
-  startCapture({ video: { facingMode: "environment" },audio:false }).then(function(stream) {
-    video.setAttribute("playsinline", true);
-    video.srcObject = stream;
-    video.play();
-    setTimeout(tick, 300);
-    scan()
-  });
-} catch (error) {
-  console.log(error)
-}
-
-
-o.add(
-  b.create.event("ojo","click",function(e,s){
-    scanning = !scanning;
-    eeeee = s
-    eeeee.innerHTML = (!scanning) ? "start" : "stop"
-  }),
-  b.create.event("close","click",function(){
-    card.hidden = true;
-  })
-)
-o.run()
-
-
-/*function uuidv4() {
-  return 'xxxy-xxxx-xxx-yxxx-xxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
-for (let i = 0; i < 4 ; i++) {
-  console.log(uuidv4())
+/*let load_config = class{
+    urls = null;
+    constructor(yaml){
+        if(!(yaml instanceof String))
+        this.yaml = yaml;
+    }
+    load = function(){
+        this._create_(YAML.load(this.yaml))
+        return this.urls
+    }
+    _error_ (a,b){
+        if(b!=null) throw Error("ERROR : " + a.replace("{}",b))
+        else throw Error("ERROR : " + a)
+    }
+    _get_can (a,b){
+        console.log(a,b)
+        return !(a[b]==null) 
+    }
+    // ::error cause
+    _get_cerr (a,b){
+        console.log(a,b)
+        if(!this._get_can(a,b)) this._error_("\"YAML\" :: {} not found in config")
+        return this._get_can(a,b);
+    }
+    _get_OBJ (a,b){
+        console.log(a,b)
+        this._get_cerr(a,b)
+        return a[b];
+    }
+    _create_ (a){
+        let textures = this._get_OBJ(a,"texture")
+        let ids = Object.keys(textures)
+        let urls = []
+        let i = 0
+        ids.forEach(e => {
+            if(e!=null)
+            if(this._get_can(textures,e))
+            urls.push(
+                new texture(i,String(this._get_OBJ(textures,e)))
+            )
+            i++;
+        });
+        this.urls = urls;
+    }
 }*/
+let ARboon = class{
+    constructor(config){
+        this.config = config;
+    }
+    _create_LIGHT (){
+        return new THREE.AmbientLight( 0xcccccc, 0.5 );
+    }
+    _create_CAMAERA (){
+        return new THREE.Camera();
+    }
+    _create_SCENE (){
+        return new THREE.Scene();
+    }
+    _create_CLOCK = function(){
+        this.clock =  new THREE.Clock();
+        this.deltaTime = 0;
+        this.totalTime = 0;
+        this.add = function(){
+            this.deltaTime = this.clock.getDelta();
+            this.totalTime += this.deltaTime;
+        }
+    }
+    _create_RUNNING_EVENT = function(ARcore,renderer,scene,camera,clock){
+        this.renderer = renderer;
+        this.arToolkitSource = ARcore.source;
+        this.arToolkitContext = ARcore.context;
 
+        let arToolkitSource  = this.arToolkitSource
+        let arToolkitContext = this.arToolkitContext
 
-/*
-7849-a625-60d-954a-b54976
-6468-39b1-586-854e-6cc455
-6748-a4b6-341-a414-ce9892
-b368-32c7-930-888d-608dc1
-*/
+        let update = function(){
+            // update artoolkit on every frame
+            if ( arToolkitSource.ready !== false )
+            arToolkitContext.update( arToolkitSource.domElement );
+        }
+        this.render = function(){
+            renderer.render( scene, camera );
+        }
+        let render = this.render
+        let animate = function(){
+            requestAnimationFrame(animate);
+            //this.clock.add()
+            update();
+            render();
+        }
+        this.animate = animate
+    }
+    _create_RENDERER (){
+        let renderer =  new THREE.WebGLRenderer({
+            antialias : true,
+            alpha: true
+        });
+        renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+        renderer.setSize( 640, 480 );
+        renderer.domElement.style.position = 'absolute'
+        renderer.domElement.style.top = '0px'
+        renderer.domElement.style.left = '0px'
+        document.body.appendChild( renderer.domElement );
+        return renderer;
+    }
+    _create_ARTHREE_Toolkit (){
+        return {
+            source:new THREEx.ArToolkitSource({
+                sourceType : 'webcam',
+            }),
+            context:new THREEx.ArToolkitContext({
+                cameraParametersUrl: 'https://stemkoski.github.io/AR-Examples/data/camera_para.dat',
+                detectionMode: 'mono'})
+        }
+    }
+    _create_ARTHREE_Toolkit_EVENT = class{
+        constructor(ARcore,camera,renderer){
+            this.ARcore = ARcore;
+            this.renderer = renderer;
+            this.arToolkitSource = ARcore.source;
+            this.arToolkitContext = ARcore.context;
+            this.camera = camera;
+        }
+        _register_ (){
+            let EVENT = this._create_EVENT_source()
+            this.ARcore.source.init(EVENT.onReady);
+            window.addEventListener('resize',EVENT.onResize);
+            this.ARcore.context.init(this._create_EVENT_context().onCompleted)
+        }
+        _create_EVENT_source(){
+            let arToolkitSource = this.arToolkitSource
+            let arToolkitContext = this.arToolkitContext
+            let renderer = this.renderer
+            let onReady = function(){
+                onResize()
+            }
+            let onResize = function(){
+                arToolkitSource.onResize()
+                arToolkitSource.copySizeTo(renderer.domElement)	
+                if (arToolkitContext.arController !== null ){
+                    arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
+                }
+            }
+            return {onReady,onResize}
+        }
+        _create_EVENT_context(){
+            let arToolkitContext = this.arToolkitContext
+            let camera = this.camera
+            let onCompleted = function(){
+                camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix());
+            }
+            return {onCompleted}
+        }
+    }
+    _init_ARTHREE_Toolkit (camera){
+        let AR = this._create_ARTHREE_Toolkit()
+        let EVENT = new this._create_ARTHREE_Toolkit_EVENT(AR,camera,this.renderer)
+        // Register event AR_camera
+        EVENT._register_()
+        return AR
+    }
+    _MARKER_ = class{
+        constructor(ARcore,scene){
+        //  (render instanceof ARboon.prototype._create_RUNNING_EVENT)
+        //  (config instanceof load_config)
+        console.log(ARcore)
+            this.ARcore = ARcore;
+            this.scene = scene;
+            this.loader = new load_texture(ARcore)
+            this.loader._create_loader()
+        }
+        _create_MARKER (){
+            return new THREE.Group();
+        }
+        _register_marker (list){
+            list.forEach(e=>{
+            //  (e instanceof texture)
+                let marker = this._create_MARKER();
+                this._create_MARKERCONTROL(marker,e.marker)
+                let texture
+                if(e.url.search(".mp4") == -1){
+                    texture = this.loader._load_buffer(e)
+                }else{
+                    let video = document.createElement("video")
+                    let source = document.createElement("source")
+                    video.loop = true;
+                    video.autoplay = true;
+                    video.crossOrigin = "anonymous"
+                    video.preload = 'auto'
+                    video.setAttribute("webkit-playsinline","")
+                    source.type = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+                    video.appendChild(source)
+                    source.src = e.url;
+                    video.src = e.url
+                    video.muted = "muted"
+                    video.load()
+                    video.play()
+                    texture = new THREE.VideoTexture( video );
+                    texture.minFilter = THREE.LinearFilter;
+                    texture.magFilter = THREE.LinearFilter;
+                    texture.format = THREE.RGBFormat;
+                    console.log(video)
+                }
+                let geometry = new THREE.PlaneBufferGeometry(1,1, 4,4);
+                let material = new THREE.MeshBasicMaterial( { map: texture } );
+                let mesh = new THREE.Mesh( geometry, material );
+                mesh.scale.set(2,2,2);
+                mesh.rotation.x = -Math.PI/2;
+                marker.add(mesh)
+                console.log(texture)
+                this.scene.add(marker)
+            })
+        }
+        _create_MARKERCONTROL (marker,markerNames){
+            let arToolkitContext = this.ARcore.arToolkitContext
+            let l = new THREEx.ArMarkerControls(arToolkitContext, marker, {
+                type: 'pattern', patternUrl: "https://raw.githubusercontent.com/s32386/s32386.github.io/main/data/pattern-" + markerNames + ".patt",
+            });
+        }
+    }
+    init(){
+        let light = this._create_LIGHT()
+        let scene = this._create_SCENE()
+        let camera = this._create_CAMAERA()
+        this.renderer = this._create_RENDERER()
+        let ARcore = this._init_ARTHREE_Toolkit(camera)
+        let OO = new this._create_RUNNING_EVENT(ARcore,this.renderer,scene,camera)
+        let marker = new this._MARKER_(OO,scene)
+        console.log(this.renderer)
+        scene.add(light)
+        scene.add(camera)
+        marker._register_marker(
+            this.config
+        )
+        OO.animate()
+    }
+}
