72f6ba2a3b7fbac3f93c2c6290b9eec74debeb66
s32386 <74299530+s32386@users.noreply.github.com>
2020-11-15 14:54:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let ar = new ARboon(
     [
-        new texture("Backstroke","https://s32386.github.io/image/kuncheang.gif"),
+        new texture("Backstroke","https://s32386.github.io/image/kuncheang.mp4"),
         new texture("Breaststroke","https://s32386.github.io/image/kob.mp4"),
-        new texture("Butterfly","https://s32386.github.io/image/peesaut.gif"),
+        new texture("Butterfly","https://s32386.github.io/image/peesaut.mp4"),
         new texture("Freestyle","https://s32386.github.io/image/free.mp4")
     ]
 )
